4f3ff7f6b9dc60dd59387fe661f94792c8f876e3
Somanyu <somanyu.03samal@gmail.com>
2021-08-04 13:35:18 +0530

    Removed unused imports

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-//import Create from './pages/Create';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
